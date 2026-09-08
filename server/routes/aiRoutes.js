848bf3f10efb64d69872877cfccdf232a90d6751
Nitanshyadav <nitanshyadavlucky@gmail.com>
2026-09-08 07:01:55 +0530

    Fix enhance job description route name

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -7,7 +7,7 @@ const aiRouter=express.Router();
 
 
 aiRouter.post('/enhance-pro-sum',protect,enhanceProfessionalSummary)
-aiRouter.post('/enhance-pro-desc',protect,enhanceJobDescription)
+aiRouter.post('/enhance-job-desc',protect,enhanceJobDescription)
 aiRouter.post('/upload-resume',protect,uploadResume)
 
 export default aiRouter;
